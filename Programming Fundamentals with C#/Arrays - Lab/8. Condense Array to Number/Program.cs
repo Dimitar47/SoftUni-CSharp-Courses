018de3510618c922943d7f07c8cafc7f80e324ca
Dimitar <dimitar_nikolov12@abv.bg>
2022-07-09 01:20:23 +0300

    Added Arrays - Lab

---
+++ Programming Fundamentals with C#/Arrays - Lab/8. Condense Array to Number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+namespace _8._Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+           
+            while (arr.Length > 1)
+            {
+
+                int[] condensed = new int[arr.Length - 1];
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    condensed[i] = arr[i] + arr[i + 1];
+                    
+                }
+                arr = condensed;
+            }
+            Console.WriteLine(arr[0]);
+        }       
+    }
+}
+
