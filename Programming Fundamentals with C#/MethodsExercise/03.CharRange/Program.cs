210af07fe80a5d9475be35a9f89e13dd5e0ba3b9
Dimitar <dimitar_nikolov12@abv.bg>
2022-07-08 22:33:08 +0300

    Added Methods - Exercise and Problems Description

---
+++ Programming Fundamentals with C#/MethodsExercise/03.CharRange/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _03.CharRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char char1 = Char.Parse(Console.ReadLine());
+            char char2 = Char.Parse(Console.ReadLine());
+          
+          PrintChars(char1,char2);
+        }
+
+       private static void PrintChars(char char1, char char2)
+        {
+            int value1 = char1;
+            int value2 = char2;
+                if (value2 < value1)
+                {
+                    for (int i = value2 + 1; i < value1; i++)
+                    {
+                        Console.Write((char)i + " ");
+                    }
+                }
+
+                else
+                {
+                    for (int i = value1 + 1; i < value2; i++)
+                    {
+                        Console.Write((char)i + " ");
+                    }
+                }
+        }
+
+    }
+}
---
+++ Programming Fundamentals with C#/MethodsExercise/1. Smallest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1._Smallest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(PrintSmallest(number1,number2,number3));
+        }
+
+        static int PrintSmallest(int number1, int number2, int number3)
+        {
+            int smallestNum = Math.Min(Math.Min(number1,number2),number3);
+            return smallestNum;
+        }
+    }
+}
