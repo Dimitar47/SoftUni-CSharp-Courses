f46bae9aebd1ee4c5e9086df38b4049fd8019502
Dimitar <dimitar_nikolov12@abv.bg>
2024-01-02 23:45:41 +0200

    Added ReflectionAndAttributes

---
+++ C# OOP/ReflectionAndAttributes - Lab/01.Stealer/Spy.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string AnalyzeAccessModifiers(string className)
+        {
+            Type type =Type.GetType("Stealer."+className);
+            StringBuilder sb = new StringBuilder();
+            FieldInfo[] fieldInfos = type.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (FieldInfo fieldInfo in fieldInfos)
+            {
+                if (!fieldInfo.IsPrivate )
+                {
+                    sb.AppendLine($"{fieldInfo.Name} must be private!");
+
+                }
+            }
+            MethodInfo[] methodInfos = type.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (MethodInfo methodInfo in methodInfos)
+            {
+                if (methodInfo.Name.ToLower().Contains("get"))
+                {
+                    if (!methodInfo.IsPublic)
+                    {
+                        sb.AppendLine($"{methodInfo.Name} have to be public!");
+                    }
+                }
+                else if (methodInfo.Name.ToLower().Contains("set"))
+                {
+                    if (!methodInfo.IsPrivate)
+                    {
+                        sb.AppendLine($"{methodInfo.Name} have to be private!");
+                    }
+                }
+
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public string StealFieldInfo(string investigationClass, params string[] fieldsToInvestigate)
+        {
+            Type type = Type.GetType(investigationClass);
+            StringBuilder sb = new StringBuilder();
+            FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            sb.AppendLine($"Class under investigation: {type.FullName}");
+            foreach (var field in fieldsToInvestigate)
+            {
+                foreach (var fieldInfo in fields)
+                {
+                    if (fieldInfo.Name == field)
+                    {
+                        Hacker hackerInstance = (Hacker) Activator.CreateInstance(type);
+                        sb.AppendLine($"{fieldInfo.Name} = {fieldInfo.GetValue(hackerInstance)}");
+                        break;
+                    }
+                }
+
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public string RevealPrivateMethods(string className)
+        {
+            Type type = Type.GetType(className);
+            MethodInfo[] methodInfos = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"All Private Methods of Class: {className}");
+            sb.AppendLine($"Base Class: {type.BaseType.Name}");
+            foreach(var methodInfo in methodInfos)
+            {
+                sb.AppendLine(methodInfo.Name);
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public string CollectGettersAndSetters(string className)
+        {
+            Type type = Type.GetType(className);
+            MethodInfo[] methodInfos = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public);
+            StringBuilder sb = new StringBuilder();
+            foreach(var methodInfo in methodInfos)
+            {
+                if (methodInfo.Name.StartsWith("get"))
+                {
+                    sb.AppendLine($"{methodInfo.Name} will return {methodInfo.ReturnType}");
+                }
+            }
+            foreach (var methodInfo in methodInfos)
+            {
+                if (methodInfo.Name.StartsWith("set"))
+                {
+                    sb.AppendLine($"{methodInfo.Name} will set field of {methodInfo.GetParameters().First().ParameterType}");
+                }
+            }
+            return sb.ToString().TrimEnd();
+
+
+        }
+    }
+}
---
+++ C# OOP/ReflectionAndAttributes - Lab/01.Stealer/StartUp.cs
@@ -0,0 +1,64 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.CollectGettersAndSetters("Stealer.Hacker");
+            Console.WriteLine(result);
+
+        }
+    }
+/*
+              Type myType = typeof(Hello); // Obtained at compile time
+
+               Type myTypeFull = Type.GetType("_01.Stealer.Hello"); // Obtained at runtime, if the name is unknown
+               string fullName = typeof(Hello).FullName; //Obtain Class name
+               string simpleName = typeof(Hello).Name;
+
+
+               Hello hello = new Hello();
+               Type baseType = hello.BaseType;
+               Type[] interfaces = hello.GetInterfaces();
+
+               var sbType = Type.GetType("System.Text.StringBuilder");
+               var sb = (StringBuilder)Activator.CreateInstance(sbType);
+               var sbCapacity = (StringBuilder)Activator.CreateInstance(sbType, new object[] { 10 });
+            
+            Type type = typeof(Hello);
+
+            Hello helloInstance = (Hello)Activator.CreateInstance(type, new object[] { "zdr" });
+            MethodInfo publicMethods = type.GetMethod("GetInterfaces", new[] {typeof(string)});
+        }
+    }
+    public interface IHello
+    {
+
+    }
+    public class Hello:IHello
+    {
+        private string name;
+        public Hello(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; private set; }
+        public Type BaseType { get; internal set; }
+
+        public Type[] GetInterfaces(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Zdr : Hello
+    {
+        public Zdr(string name) : base(name)
+        {
+        }
+    }
+    */
+}
