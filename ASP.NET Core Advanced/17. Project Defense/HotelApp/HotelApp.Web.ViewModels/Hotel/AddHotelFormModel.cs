e929aef2f45b903c394038b10fe50255c9b1cac8
Dimitar <dimitar_nikolov12@abv.bg>
2024-12-02 19:47:44 +0200

    Added Hotel Create Functionality

---
+++ ASP.NET Core Advanced/17. Project Defense/HotelApp/HotelApp.Web.ViewModels/Hotel/AddHotelFormModel.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static HotelApp.Common.EntityValidationConstants.Hotel;
+using static HotelApp.Common.EntityValidationMessages.Hotel;
+
+
+
+namespace HotelApp.Web.ViewModels.Hotel
+{
+    public class AddHotelFormModel
+    {
+        public AddHotelFormModel()
+        {
+            CheckinTime = new TimeSpan(14, 0, 0).ToString(CheckInOutTimeSpanFormat);
+            CheckoutTime = new TimeSpan(11, 0, 0).ToString(CheckInOutTimeSpanFormat);
+        }
+
+
+        [Required(ErrorMessage = NameRequiredMessage)]
+        [MinLength(HotelNameMinLength)]
+        [MaxLength(HotelNameMaxLength)]
+        public string Name { get; set; } = null!;
+
+
+        [Required(ErrorMessage = AddressRequiredMessage)]
+        [MaxLength(HotelAddressMaxLength)]
+        public string Address { get; set; } = null!;
+
+        [Display(Name ="Image URL")]
+        public string? ImageURL { get; set; }
+
+        [Required(ErrorMessage = PhoneRequiredMessage)]
+        [Phone]
+        [MaxLength(HotelPhoneMaxLength)]
+        public string Phone { get; set; } = null!;
+
+        [Required(ErrorMessage = EmailRequiredMessage)]
+        [EmailAddress]
+        [MaxLength(HotelEmailMaxLength)]
+        public string Email { get; set; } = null!;
+
+
+        [Required(ErrorMessage = StarsRequiredMessage)]
+        [Range(HotelStarsMinLength,HotelStarsMaxLength)]
+        public int Stars { get; set; }
+
+        [Required(ErrorMessage = CheckinTimeRequiredMessage)]
+        [Display(Name= "Check-in time")]
+        public string CheckinTime { get; set; }
+
+        [Required(ErrorMessage = CheckoutTimeRequiredMessage)]
+        [Display(Name = "Check-out time")]
+        public string CheckoutTime { get; set; }
+    }
+}
--- ASP.NET Core Advanced/17. Project Defense/HotelApp/HotelApp.Web/Controllers/HotelController.cs
+++ ASP.NET Core Advanced/17. Project Defense/HotelApp/HotelApp.Web/Controllers/HotelController.cs
@@ -38,6 +38,60 @@ namespace HotelApp.Web.Controllers
             return View(allHotels);
         }
 
-        
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AddHotelFormModel inputModel)
+        {
+
+            bool isCheckinTimeValid = TimeSpan.TryParseExact(inputModel.CheckinTime, CheckInOutTimeSpanFormat, CultureInfo.InvariantCulture, TimeSpanStyles.None, out TimeSpan validCheckinTime);
+
+            if (!isCheckinTimeValid)
+            {
+                this.ModelState.AddModelError(nameof(inputModel.CheckinTime),
+                    String.Format("The Check-in Time must be in the following format: {0}", CheckInOutTimeSpanFormat));
+            }
+
+
+            bool isCheckoutTimeValid = TimeSpan.TryParseExact(inputModel.CheckoutTime, CheckInOutTimeSpanFormat, CultureInfo.InvariantCulture, TimeSpanStyles.None, out TimeSpan validCheckoutTime);
+
+            if (!isCheckoutTimeValid)
+            {
+                this.ModelState.AddModelError(nameof(inputModel.CheckoutTime),
+                    String.Format("The Check-out Time must be in the following format: {0}", CheckInOutTimeSpanFormat));
+            }
+
+
+            if (!this.ModelState.IsValid)
+            {
+                //Render the same form with user entered values + model errors
+                return this.View(inputModel);
+            }
+
+
+            Hotel hotel = new Hotel()
+            {
+                Name = inputModel.Name,
+                Address = inputModel.Address,
+                ImageURL = inputModel.ImageURL,
+                Phone = inputModel.Phone,
+                Email = inputModel.Email,
+                Stars = inputModel.Stars,
+                CheckinTime = validCheckinTime,
+                CheckoutTime = validCheckoutTime
+            };
+
+
+            await this.dbContext.Hotels.AddAsync(hotel);
+            await this.dbContext.SaveChangesAsync();
+
+            return this.RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
