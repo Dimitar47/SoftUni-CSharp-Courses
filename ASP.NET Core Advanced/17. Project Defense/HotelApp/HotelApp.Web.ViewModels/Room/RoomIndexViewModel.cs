e9317eda05470614e9a4b534dbb8193e8dc5a961
Dimitar <dimitar_nikolov12@abv.bg>
2024-12-03 23:25:19 +0200

    Added Room Index Functionality

---
+++ ASP.NET Core Advanced/17. Project Defense/HotelApp/HotelApp.Web.ViewModels/Room/RoomIndexViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelApp.Web.ViewModels.Room
+{
+    public class RoomIndexViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public int RoomNumber { get; set; }
+
+        public string Status { get; set; } = null!;
+
+        public string? ImageURL { get; set; }
+
+        public string HotelName { get; set; } = null!;
+
+        public string HotelAddress { get; set; } = null!;
+
+        public string RoomTypeName { get; set; } = null!;
+       
+    }
+}
---
+++ ASP.NET Core Advanced/17. Project Defense/HotelApp/HotelApp.Web/Controllers/RoomController.cs
@@ -0,0 +1,66 @@
+﻿using HotelApp.Data;
+using HotelApp.Web.ViewModels.Room;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelApp.Web.Controllers
+{
+    public class RoomController : BaseController
+    {
+        private readonly HotelDbContext dbContext;
+
+        public RoomController(HotelDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        //Perfect
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+
+            var roomAssociations = await dbContext.HotelsRooms
+                                     .Include(hr => hr.Hotel)
+                                     .Include(hr => hr.Room)
+                                     .ThenInclude(r => r.RoomType)
+                                     .Where(hr => !hr.IsDeleted &&
+                                             !hr.Room.IsDeleted &&
+                                             !hr.Hotel.IsDeleted)
+                                     .Select(hr => new RoomIndexViewModel
+                                     {
+                                         Id = hr.Room.Id.ToString(),
+                                         RoomNumber = hr.Room.RoomNumber,
+                                         Status = hr.Room.Status,
+                                         ImageURL = hr.Room.ImageURL,
+                                         RoomTypeName = hr.Room.RoomType.Name,
+                                         HotelName = hr.Hotel != null ? hr.Hotel.Name : "Unassociated",
+                                         HotelAddress = hr.Hotel != null ? hr.Hotel.Address : "N/A"
+                                     })
+                                     .ToListAsync();
+
+
+            var unassociatedRooms = await dbContext.Rooms
+                .Include(r => r.RoomType)
+                .Where(r => !r.IsDeleted &&
+                       !r.RoomHotels.Any(rh =>
+                           !rh.IsDeleted && !rh.Hotel.IsDeleted))
+                .Select(r => new RoomIndexViewModel
+                {
+                    Id = r.Id.ToString(),
+                    RoomNumber = r.RoomNumber,
+                    Status = r.Status,
+                    ImageURL = r.ImageURL,
+                    RoomTypeName = r.RoomType.Name,
+                    HotelName = "Unassociated",
+                    HotelAddress = "N/A"
+                })
+                .ToListAsync();
+
+            var rooms = roomAssociations.Concat(unassociatedRooms)
+                               .OrderBy(r => r.RoomNumber)
+                               .ToList();
+
+            return this.View(rooms);
+        }
+
+    }
+}
